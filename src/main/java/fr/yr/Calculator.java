3c4c74a5a8daffd8ced93a41cc7cd6735999fcc1
Youssef59800 <rguibi.youssef@gmail.com>
2023-12-11 19:10:10 +0100

    demo test for AquaO

---
+++ src/main/java/fr/yr/Calculator.java
@@ -0,0 +1,15 @@
+package fr.yr;
+
+/**
+ * @author: YOUSSEF RGUIBI
+ * @date: 11/12/2023
+ * @time: 15:50
+ * @project_name: test-auto
+ */
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/fr/yr/Main.java
@@ -0,0 +1,7 @@
+package fr.yr;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
